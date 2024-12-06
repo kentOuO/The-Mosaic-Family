9001bd4f8755aadb199b82df393cfee669db77ca
kentOuO <kenttan0918@gmail.com>
2024-12-06 14:05:00 +0800

    update spam eat

--- Assets/Script/Shower.cs
+++ Assets/Script/Shower.cs
@@ -2,41 +2,60 @@ using UnityEngine;
 
 public class Shower : MonoBehaviour
 {
-    public ParticleSystem rainEffect; // Reference to the Particle System
+    public ParticleSystem rainEffect1; // Reference to the first Particle System
+    public ParticleSystem rainEffect2; // Reference to the second Particle System
 
     private void Start()
     {
-        // Hide the Particle System at the start
-        if (rainEffect != null)
+        // Hide both Particle Systems at the start
+        if (rainEffect1 != null)
         {
-            rainEffect.gameObject.SetActive(false);
+            rainEffect1.gameObject.SetActive(false);
+        }
+        if (rainEffect2 != null)
+        {
+            rainEffect2.gameObject.SetActive(false);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Check if the colliding object has the "Character A" tag
+        // Check if the colliding object has the "CharacterA" tag
         if (collision.CompareTag("CharacterA"))
         {
-            // Enable and play the Particle System
-            if (rainEffect != null)
+            // Enable and play the first Particle System
+            if (rainEffect1 != null)
             {
-                rainEffect.gameObject.SetActive(true);
-                rainEffect.Play();
+                rainEffect1.gameObject.SetActive(true);
+                rainEffect1.Play();
+            }
+
+            // Enable and play the second Particle System
+            if (rainEffect2 != null)
+            {
+                rainEffect2.gameObject.SetActive(true);
+                rainEffect2.Play();
             }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        // Check if the colliding object has the "Character A" tag
+        // Check if the colliding object has the "CharacterA" tag
         if (collision.CompareTag("CharacterA"))
         {
-            // Stop and hide the Particle System
-            if (rainEffect != null)
+            // Stop and hide the first Particle System
+            if (rainEffect1 != null)
             {
-                rainEffect.Stop();
-                rainEffect.gameObject.SetActive(false);
+                rainEffect1.Stop();
+                rainEffect1.gameObject.SetActive(false);
+            }
+
+            // Stop and hide the second Particle System
+            if (rainEffect2 != null)
+            {
+                rainEffect2.Stop();
+                rainEffect2.gameObject.SetActive(false);
             }
         }
     }
